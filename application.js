ca221ed6750cd331951d1154c1098a9e472ae1ec
crazybbs <moi@mathieugenereux.com>
2017-09-04 18:08:02 -0400

    20

--- application.js
+++ application.js
@@ -281,7 +281,7 @@ define(['globalize', 'loading', 'appSettings', 'focusManager', 'scrollHelper', '
             
             if (player.path == 'c:\\windows\\system32\\cmd.exe' && player.arguments[0] =='/c') {
                 player.path = player.arguments[1];
-
+                player.arguments.splice(0, 2);
             }
 
 
