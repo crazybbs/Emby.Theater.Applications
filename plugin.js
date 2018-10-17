271ffab85fe966f6703c1db3bea121f8a94e5d59
crazybbs <moi@mathieugenereux.com>
2018-10-17 15:24:36 -0400

    115

--- plugin.js
+++ plugin.js
@@ -194,7 +194,7 @@ define(['events', 'appSettings', 'pluginManager', 'packageManager', 'shell', 'fi
         self.play = function (options) {
 
             var player = getPlayer(options);
-
+            console.log(player);
             var path = player.path;
 
             return modifyStreamUrl(options).then(function (streamUrl) {
