a1aaa89b3d1f6e8555fefc0d03c4f26cbe453704
crazybbs <moi@mathieugenereux.com>
2019-01-18 15:21:42 -0500

    176

--- plugin.js
+++ plugin.js
@@ -72,6 +72,8 @@ define(['events', 'appSettings', 'pluginManager', 'packageManager', 'shell', 'fi
         }
 
         function isConfiguredToPlay(player, options) {
+            alert(player.gameSystem);
+            alert(options.gameSystem);
             if (!shell.canExec) {
                 return false;
             }
